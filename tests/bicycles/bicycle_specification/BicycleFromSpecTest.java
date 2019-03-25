9c89c775aee14212c16d25e7228339782067b8f4
yegan <yeganj@gmail.com>
2019-03-25 10:36:13 +0200

    name change

--- tests/bicycles/bicycle_specification/BicycleFromSpecTest.java
+++ tests/bicycles/bicycle_specification/BicycleFromSpecTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BikeSpecTest {
+public class BicycleFromSpecTest {
 
     @Test
     public void BikeSpecTestMountainBike(){
